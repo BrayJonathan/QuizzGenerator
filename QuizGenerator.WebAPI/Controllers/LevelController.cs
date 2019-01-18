6050ef017b551e141f540881b4090989ffaa1a92
Administrateur <jonathan.bray0@gmail.com>
2019-01-18 10:03:21 +0100

    Add WebApi project to angular

---
+++ QuizGenerator.WebAPI/Controllers/LevelController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using QuizzGenerator.Domain.Entities;
+
+namespace QuizGenerator.WebAPI.Controllers
+{
+    public class LevelController : ApiController
+    {
+        private QuizContext db = new QuizContext();
+
+        // GET: api/Levels
+        //public IQueryable<Level> GetLevels()
+        //{
+        //    return db.Levels;
+        //}
+
+        //GET: api/Level
+        [ResponseType(typeof(IEnumerable<Level>))]
+        public async Task<IHttpActionResult> GetLevels()
+        {
+            return  Ok(db.Levels.ToList());
+        }
+
+        // GET: api/Levels/5
+        [ResponseType(typeof(Level))]
+        public async Task<IHttpActionResult> GetLevel(int id)
+        {
+            Level level = await db.Levels.FindAsync(id);
+            if (level == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(level);
+        }
+
+        // PUT: api/Levels/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutLevel(int id, Level level)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != level.LevelID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(level).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LevelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Levels
+        [ResponseType(typeof(Level))]
+        public async Task<IHttpActionResult> PostLevel(Level level)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Levels.Add(level);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = level.LevelID }, level);
+        }
+
+        // DELETE: api/Levels/5
+        [ResponseType(typeof(Level))]
+        public async Task<IHttpActionResult> DeleteLevel(int id)
+        {
+            Level level = await db.Levels.FindAsync(id);
+            if (level == null)
+            {
+                return NotFound();
+            }
+
+            db.Levels.Remove(level);
+            await db.SaveChangesAsync();
+
+            return Ok(level);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LevelExists(int id)
+        {
+            return db.Levels.Count(e => e.LevelID == id) > 0;
+        }
+    }
+}
---
+++ QuizGenerator.WebAPI/Controllers/QuestionController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using QuizzGenerator.Domain.Entities;
+
+namespace QuizGenerator.WebAPI.Controllers
+{
+    public class QuestionController : ApiController
+    {
+        private QuizContext db = new QuizContext();
+
+        // GET: api/Question
+        public IQueryable<Question> GetQuestions()
+        {
+            return db.Questions;
+        }
+
+        // GET: api/Question/5
+        [ResponseType(typeof(Question))]
+        public async Task<IHttpActionResult> GetQuestion(int id)
+        {
+            Question question = await db.Questions.FindAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(question);
+        }
+
+        // PUT: api/Question/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutQuestion(int id, Question question)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != question.QuestionId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(question).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Question
+        [ResponseType(typeof(Question))]
+        public async Task<IHttpActionResult> PostQuestion(Question question)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Questions.Add(question);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = question.QuestionId }, question);
+        }
+
+        // DELETE: api/Question/5
+        [ResponseType(typeof(Question))]
+        public async Task<IHttpActionResult> DeleteQuestion(int id)
+        {
+            Question question = await db.Questions.FindAsync(id);
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            db.Questions.Remove(question);
+            await db.SaveChangesAsync();
+
+            return Ok(question);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool QuestionExists(int id)
+        {
+            return db.Questions.Count(e => e.QuestionId == id) > 0;
+        }
+    }
+}
